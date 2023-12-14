0662aa5b11e6f7990daa344194bb77d041194e45
robin <robinhahn99@outlook.de>
2023-12-14 10:44:21 +0100

    board visual test Jshell

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -1,8 +1,10 @@
 package Model;
 
+import java.util.Arrays;
+
 public class Model {
 
-    private final String EMPTY = "-";
+    private final String EMPTY = "⌗";
 
     private final String PLAYER_1 = "■";
     private final String PLAYER_2 = "□";
@@ -29,50 +31,36 @@ public class Model {
 
     public void newGame() {
         board = new String[]{
-                EMPTY,"-----", EMPTY, "-----", EMPTY, //first row
-                "|",            "|",            "|",
-                EMPTY, EMPTY, EMPTY, //second row
-                EMPTY, EMPTY, EMPTY, //third row
-                EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, //middle row
-                EMPTY, EMPTY, EMPTY, //third row
-                EMPTY, EMPTY, EMPTY, //second row
-                EMPTY, EMPTY, EMPTY //first row
+                EMPTY,"------------------",EMPTY,"-------------------",EMPTY + "\n",
+                 "|",                       "|",                        "|"  + "\n",
+                 "|",         EMPTY,       EMPTY,       EMPTY,          "|"  + "\n",
+                 "|",          "|",         "|",         "|",           "|"  + "\n",
+                 "|",          "|", EMPTY, EMPTY, EMPTY, "|",           "|"  + "\n",
+                 "|",          "|",  "|",          "|",  "|",           "|"  + "\n",
+                EMPTY      ,  EMPTY,EMPTY,        EMPTY,EMPTY,         EMPTY + "\n",
+                 "|",          "|",  "|",          "|",  "|",           "|"  + "\n",
+                 "|",          "|", EMPTY, EMPTY, EMPTY, "|",           "|"  + "\n",
+                 "|",          "|",         "|",         "|",           "|"  + "\n",
+                 "|",         EMPTY,       EMPTY,       EMPTY,          "|"  + "\n",
+                 "|",                       "|",                        "|"  + "\n",
+                EMPTY,"------------------",EMPTY,"------------------", EMPTY + "\n"
         };
 
 
         moves = 0;
-    }
-
-    private void initGame(){
-        for (char c: board) {
-
-        }
+        System.out.println(Arrays.toString(board));
     }
 
 
-    public char getField(int field){
-        if(isValidFieldIndex(field)){
-            return board[field];
-        } else {
-            throw new IndexOutOfBoundsException("No Valid Index!");
-        }
-    }
+
+
+
 
     private boolean isValidFieldIndex(int field){
         return field >= 0 && field < board.length;
     }
 
-    public char playerMove(){
-        return (moves % 2 == 0) ? PLAYER_1 : PLAYER_2;
-    }
 
-    public void move(int field){
-        if (!isGameOver){
-            if (isValidFieldIndex(field)){
-
-            }
-        }
-    }
 
 
 
