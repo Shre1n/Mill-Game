a9e5445da3a63c6f763daacfa05240579b21624b
robin <robin.hahn@mni.thm.de>
2024-01-23 20:10:38 +0100

    typo fixed

--- Model_Jshell/package-info.java
+++ Model_Jshell/package-info.java
@@ -2,6 +2,6 @@
  * @author Robin Hahn
  * @version 1.0
  * @since 1.0
- * This Package copied the actual Model Code to run run the model in Jshell.
+ * This Package copied the actual Model Code to run the model in Jshell.
  *
  */
